ea49a964161f11891337f73d14bcf5a3080270d1
tommy-adams <thomasadams012@gmail.com>
2024-02-05 00:47:03 -0800

    fixed 403 bug, auth changes

--- frontend/src/api/personal.js
+++ frontend/src/api/personal.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+import { authorize } from "./auth";
 
 export const fetch_personal_info = async () => {
+  await authorize();
+  
   const token = Cookies.get('token');
-  if (token === undefined) return;
   const config = {
     headers: { Authorization: `Bearer ${token}`}
   };
-  const data = await axios.get("https://api.spotify.com/v1/me", config);
-  return data;
+
+  try {
+    const data = await axios.get("https://api.spotify.com/v1/me", config);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 };
