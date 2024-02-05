ba13e1f4b87c91367cdfe6292e589cff7bb4ddf6
gcockroft <77020027+gcockroft@users.noreply.github.com>
2024-02-04 17:44:09 -0800

    Added React Router
    Basic routes setup for Landing, Analysis, Curator, pages. Refactored landing elements into Landing component out of App

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { authorize } from "../api/auth";
+import { fetch_personal_info } from "../api/personal";
 
-const NavBar = ({ auth }) => {
+const NavBar = () => {
+  const [auth, setAuth] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    authorize(false).then((res) => setAuth(res));
+  }, []);
+
+  useEffect(() => {
+    if (auth) {
+      setLoading(true);
+      fetch_personal_info().then((res) => console.log(res.data));
+      setLoading(false);
+    }
+  }, [auth]);
+
   const onLogin = () => {
-    authorize(true);
+    authorize(true).then((res) => setAuth(res));
   };
 
-  return (
-    <div className="w-full h-14 bg-red-400 flex px-6 py-2">
-      <p className="font-extrabold text-white text-3xl">moosh.</p>
+  return loading ? (
+    <div>Loading</div>
+  ) : (
+    <div className="w-full h-14 bg-[#FFFFFF]/[.2] flex px-6 py-2 border-b-1 border-[#C7C7C7]">
+      <p className="font-extrabold text-red-400 text-3xl">moosh.</p>
       <div className="w-full flex justify-end items-center">
         {!auth && (
           <div
