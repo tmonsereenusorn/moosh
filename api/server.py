03e629f5b7195e9eab2ff0bb03b78abd779505cc
Jonathan Coronado <jonathan@DNa1d61c1.SUNet>
2024-02-06 00:23:57 -0800

    Initial spotipy integration

--- api/server.py
+++ api/server.py
@@ -3,6 +3,8 @@ Moosh Flask Server.
 """
 from flask import Flask, request
 from dotenv import load_dotenv
+from model import query_openai
+from spotify_api import spotipy_search
 
 app = Flask(__name__)
 load_dotenv()
@@ -31,6 +33,30 @@ def prompt_openai():
   except:
     return "Error prompting OpenAI", 500
   
+@app.route("/search", methods=["GET"])
+def search_spotify():
+  """
+  Search spotify for item ids.
+  Headers
+    Authorization: Bearer <token>
+  Request Body:
+    q: str
+    type: List[str]
+  """  
+  body = request.json
+  # expecting a well-formed query
+  q = body.get('q','')
+  search_type = body.get('type','')
+  authorization = body.get('Authorization','')
+
+  if not authorization:
+    return "Error: Missing Authorization", 401
+  try:
+    return spotipy_search(q, search_type)
+  except:
+    return "Error searching Spotify", 500
+
+
 
 if __name__ == "__main__":
   app.run()
