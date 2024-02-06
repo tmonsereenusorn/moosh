a72707d6a63bb0c3958f15fd25be16d325d9cba3
Jonathan Coronado <jonathan@DN0a24988d.SUNet>
2024-02-06 15:01:51 -0800

    Add basic search and user lookup through spotipy

--- api/server.py
+++ api/server.py
@@ -4,7 +4,7 @@ Moosh Flask Server.
 from flask import Flask, request
 from dotenv import load_dotenv
 from model import query_openai
-from spotify_api import spotipy_search
+from spotify_api import spotipy_search, get_user
 
 app = Flask(__name__)
 load_dotenv()
@@ -33,12 +33,28 @@ def prompt_openai():
   except:
     return "Error prompting OpenAI", 500
   
+@app.route("/user", methods=["GET"])
+def get_spotify_user():
+  """
+  Get basic info about a spotify account
+  Headers:
+    Content-Type: application/json
+  Request Body:
+    user_id: str
+  """
+  body = request.json
+  user = body.get('user_id','')
+  try:
+    return get_user(user)
+  except:
+    return "Error querying Spotify", 500
+  
 @app.route("/search", methods=["GET"])
 def search_spotify():
   """
   Search spotify for item ids.
-  Headers
-    Authorization: Bearer <token>
+  Headers:
+    Content-Type: application/json
   Request Body:
     q: str
     type: List[str]
@@ -47,10 +63,7 @@ def search_spotify():
   # expecting a well-formed query
   q = body.get('q','')
   search_type = body.get('type','')
-  authorization = body.get('Authorization','')
 
-  if not authorization:
-    return "Error: Missing Authorization", 401
   try:
     return spotipy_search(q, search_type)
   except:
