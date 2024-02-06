3ae29c749ed61add3e3653a5b71e05e58ae544be
Jonathan Coronado <jonathan@DN0a24988d.SUNet>
2024-02-06 15:01:51 -0800

    Add basic search and user lookup through spotipy

--- api/spotify_api.py
+++ api/spotify_api.py
@@ -1,10 +1,20 @@
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify()
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-def spotipy_search(query, type):
+def get_user(user):
     try:
-        response = sp.search(q=query, type=type)
+        response = sp.user(user)
+        return response
+    except Exception as e:
+        print("Unable to get user from Spotify")
+        print(f"Exception: {e}")
+        return e
+
+def spotipy_search(query, type, limit=1):
+    try:
+        response = sp.search(q=query, type=type, limit=limit)
         return response
     except Exception as e:
         print("Unable to search Spotify")
