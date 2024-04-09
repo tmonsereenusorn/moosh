be39394f51b430ab5759d699d4e9ef00d1691ccf
gcockroft <77020027+gcockroft@users.noreply.github.com>
2024-04-08 23:49:53 -0700

    Right justified popover trigger

--- frontend/src/components/History/HistoryItem.jsx
+++ frontend/src/components/History/HistoryItem.jsx
@@ -23,19 +23,20 @@ const HistoryItem = ({ text, onClick, timestamp, promptId, playlistRef }) => {
       onClick={() => onClick()}
     >
       <div className="absolute left-0 right-1 h-8 bg-gradient-to-l from-gray-100 from-0% via-white/[0.01] via-90% hover:cursor-pointer" />
-      <p className="text-sm whitespace-nowrap overflow-x-hidden font-semibold">
+      <p className="flex-grow text-sm whitespace-nowrap overflow-x-hidden font-semibold">
         {text}
       </p>
       <Popover position="right">
         <Popover.Trigger>
           {active && (
-            <BsThreeDots
-              className="relative z-30 scale-200 hover:text-primary hover:cursor-pointer text-surface"
-            />
+            <BsThreeDots className="relative z-30 scale-200 hover:text-primary hover:cursor-pointer text-surface" />
           )}
         </Popover.Trigger>
         <Popover.Content>
-          <div className="rounded-md p-1 hover:cursor-pointer hover:bg-gray-200 flex space-x-2" onClick={e => action(e, onDelete)}>
+          <div
+            className="rounded-md p-1 hover:cursor-pointer hover:bg-gray-200 flex space-x-2"
+            onClick={(e) => action(e, onDelete)}
+          >
             <MdDelete />
             <p className="text-xs">delete</p>
           </div>
