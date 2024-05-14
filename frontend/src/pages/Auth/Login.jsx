0d70dc92d0d9e9c34362d593da9f89f3d27f543a
Jonathan Coronado <jcoronad@stanford.edu>
2024-05-13 20:17:09 -0700

    Refactor login with mobile viewports

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -51,16 +51,16 @@ const Login = () => {
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-gradient-to-br from-gradientStart via-gradientMiddle/[.8] to-gradientEnd/[.56] overflow-hidden h-screen">
       <div className="flex justify-center items-center h-screen">
-        <div className="w-1/4 space-y-4">
+        <div className="w-3/4 md:w-1/4 space-y-4 ">
           <p className="text-5xl font-bold text-white">login.</p>
           <Input placeholder="email" textColor="white" onChange={e => setEmail(e.target.value)} />
           <Input placeholder="password" type="password" textColor="white" onChange={e => setPassword(e.target.value)} onKeyDown={handleEnter} />
           <div className="flex w-full items-center justify-end">
             <ButtonPrimary text="continue" onClick={() => login()} disabled={disabled} />
           </div>
-          {error && <p className="text-red-500">invalid credentials.</p>}
+          {error && <p className="text-center md:text-start text-red-500">invalid credentials.</p>}
           <div className="text-center">
-            <p className="text-white text-xs">not a member yet? <span className="hover:underline hover:cursor-pointer" onClick={() => navigate("/signup")}>sign up.</span></p>
+            <p className="text-white text-md md:text-xs">not a member yet? <span className="hover:underline hover:cursor-pointer" onClick={() => navigate("/signup")}>sign up.</span></p>
           </div>
         </div>
       </div>
