1dabb262da017472f8a40914ab01faf81e1b655d
Tee Monsereenusorn <tmonsereenusorn@gmail.com>
2024-06-03 20:22:17 -0700

    fixed width of synopsis bar

--- frontend/src/components/Curator/views/CuratedView.jsx
+++ frontend/src/components/Curator/views/CuratedView.jsx
@@ -28,62 +28,65 @@ const CuratedView = ({
 }) => {
   return (
     <>
-      <div className="flex flex-col w-3/4 h-screen pt-14 pb-36 sm:pb-24">
+      <div className="flex flex-col items-center w-full md:w-1/2 h-screen pt-14 pb-36 sm:pb-24 mx-auto">
         {recs.length > 0 && (
-          <div className="w-full items-center justify-center p-2">
-            <div className="text-2xl font-bold text-surface text-center">
-              {prompt}
-            </div>
-            <div className="text-m text-surface text-center border border-gray-300 p-3 rounded mb-3 mt-2 bg-gray-100">
-              <div className="mb-3">
-                {synopsis}
+          <>
+            <div className="w-full items-center justify-center p-2">
+              <div className="text-2xl font-bold text-surface text-center">
+                {prompt}
               </div>
-              <RepromptInput
-                onSubmit={() => {
-                  onSubmit(false, true);
-                }}
-                value={reprompt}
-                onChangeText={(event) => onChangeReprompt(event)}
-                disabled={reprompt.length === 0}
-              />
-            </div>
-            <div className="flex justify-left items-center pl-2 md:pl-4">
-              <Checkbox
-                size="lg"
-                colorScheme="dark_accent"
-                onChange={toggleSelectAllButton}
-                isChecked={selectAllButton}
-              />
-              <p className="font-bold text-sm text-surface ml-3">
-                {getSelectedCount()} selected
-              </p>
-            </div>
-          </div>
-        )}
-        <div className="overflow-y-auto sm:mx-56">
-          <AudioProvider>
-            {recs.map((recommendation) => {
-              return (
-                <TrackCard
-                  key={recommendation.id}
-                  artist={recommendation.artist}
-                  title={recommendation.title}
-                  duration={recommendation.duration}
-                  preview={recommendation.preview}
-                  uri={recommendation.uri}
-                  url={recommendation.url}
-                  isSelected={selectedTracks[recommendation.id]}
-                  isNew={recommendation.isNew}
-                  onToggleSelection={() =>
-                    toggleTrackSelection(recommendation.id)
-                  }
-                  previewCallback={previewCallback}
-                  linkCallback={linkCallback}
+              <div className="text-m text-surface text-center border border-gray-300 p-3 rounded mb-3 mt-2 bg-gray-100 w-full mx-auto">
+                <div className="mb-3">
+                  {synopsis}
+                </div>
+                <RepromptInput
+                  onSubmit={() => {
+                    onSubmit(false, true);
+                  }}
+                  value={reprompt}
+                  onChangeText={(event) => onChangeReprompt(event)}
+                  disabled={reprompt.length === 0}
+                  className="w-full"
                 />
-              );
-            })}
-          </AudioProvider>
-        </div>
+              </div>
+              <div className="flex justify-left items-center w-full mx-auto">
+                <Checkbox
+                  size="lg"
+                  colorScheme="dark_accent"
+                  onChange={toggleSelectAllButton}
+                  isChecked={selectAllButton}
+                />
+                <p className="font-bold text-sm text-surface ml-3">
+                  {getSelectedCount()} selected
+                </p>
+              </div>
+            </div>
+            <div className="overflow-y-auto w-full mx-auto">
+              <AudioProvider>
+                {recs.map((recommendation) => {
+                  return (
+                    <TrackCard
+                      key={recommendation.id}
+                      artist={recommendation.artist}
+                      title={recommendation.title}
+                      duration={recommendation.duration}
+                      preview={recommendation.preview}
+                      uri={recommendation.uri}
+                      url={recommendation.url}
+                      isSelected={selectedTracks[recommendation.id]}
+                      isNew={recommendation.isNew}
+                      onToggleSelection={() =>
+                        toggleTrackSelection(recommendation.id)
+                      }
+                      previewCallback={previewCallback}
+                      linkCallback={linkCallback}
+                    />
+                  );
+                })}
+              </AudioProvider>
+            </div>
+          </>
+        )}
       </div>
       <div
         className="fixed bottom-0 flex w-full md:w-2/3 bg-white items-center justify-center p-2 md:p-8 space-x-4"
